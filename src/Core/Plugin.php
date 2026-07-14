75487f6dd67d7d32bf234569676b2c8e5002f07f
Felix Cohen <felix@felixcohen.co.uk>
2026-07-14 15:49:47 +0100

    feat: write manifests before the cron-triggered bundle rebuild

--- src/Core/Plugin.php
+++ src/Core/Plugin.php
@@ -14,6 +14,7 @@ use Tclp\WpMarkdownForAgents\Discovery\ArdCatalog;
 use Tclp\WpMarkdownForAgents\Generator\BundleGenerator;
 use Tclp\WpMarkdownForAgents\Generator\ContentFilter;
 use Tclp\WpMarkdownForAgents\Generator\Converter;
+use Tclp\WpMarkdownForAgents\Generator\ExportPolicy;
 use Tclp\WpMarkdownForAgents\Generator\FieldResolver;
 use Tclp\WpMarkdownForAgents\Generator\FileWriter;
 use Tclp\WpMarkdownForAgents\Generator\FrontmatterBuilder;
@@ -149,7 +150,15 @@ class Plugin {
 		$this->loader->add_action( 'markdown_for_agents_file_deleted', $bundle_generator, 'mark_stale_and_schedule' );
 		$this->loader->add_action( 'markdown_for_agents_taxonomy_file_generated', $bundle_generator, 'mark_stale_and_schedule' );
 		$this->loader->add_action( 'markdown_for_agents_taxonomy_file_deleted', $bundle_generator, 'mark_stale_and_schedule' );
-		$this->loader->add_action( 'markdown_for_agents_rebuild_bundle', $bundle_generator, 'on_rebuild_bundle' );
+		$rebuild_with_manifest = function () use ( $generator, $bundle_generator, $options ): void {
+			$generator->write_manifests(
+				Options::get_export_base( $options ),
+				ExportPolicy::enabled_post_types( $options )
+			);
+			$bundle_generator->on_rebuild_bundle();
+		};
+
+		$this->loader->add_action( 'markdown_for_agents_rebuild_bundle', $rebuild_with_manifest, '__invoke' );
 	}
 
 	/**
